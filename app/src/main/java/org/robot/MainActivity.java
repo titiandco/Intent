9b9d4c031bb9b8baf13aa99234a7f11cb0e73391
34011-73-10 <placidolopez123@hotmail.com>
2016-10-19 09:24:32 +0200

    mercredi 19 oct

--- app/src/main/java/org/robot/MainActivity.java
+++ app/src/main/java/org/robot/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 
+
 public class MainActivity extends Activity {
 
 	@Override
---
+++ app/src/main/java/org/robot/ThirdActivity.java
@@ -0,0 +1,8 @@
+package org.robot;
+
+/**
+ * Created by 34011-73-10 on 19/10/2016.
+ */
+
+public class ThirdActivity {
+}
